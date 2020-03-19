160d31d55b97474fc3876f5e4b1d97790d8171dd
risrael-1 <rapha.israel@live.fr>
2020-03-19 15:56:55 +0100

    add router and page for ville

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,10 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Node App' });
+});
+router.post('/ville', function(req, res, next) {
+  res.render('ville', { ville: req.body.ville });
 });
 
 module.exports = router;
